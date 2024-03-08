6c671dbb3832b201628b4f88397aba366098af1d
Suren <surendransaha@gmail.com>
2024-03-09 02:40:06 +0530

    CWD wrong code removed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Team from './components/Team';
+
+function App() {
+  return (
+    <div className="App">
+      <Team />
+    </div>
+  );
+}
+
+export default App;
